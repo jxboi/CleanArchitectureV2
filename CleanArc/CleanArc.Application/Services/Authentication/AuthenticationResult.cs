9d0e5976fd0d01c86406ac79d5da000ffd5bb1a5
Jia Xing <jia_87@hotmail.com>
2023-04-25 03:32:54 +0000

    new authentication result

---
+++ CleanArc/CleanArc.Application/Services/Authentication/AuthenticationResult.cs
@@ -0,0 +1,8 @@
+namespace CleanArc.Application.Services.Authentication;
+
+public record AuthenticationResult(
+    Guid Id,
+    string FirstName, 
+    string LastName, 
+    string Email, 
+    string Token);
