bd0d58602332e0c59487d56f73ee86c755207424
Jia Xing <jia_87@hotmail.com>
2023-04-25 03:32:45 +0000

    new authentication service

---
+++ CleanArc/CleanArc.Application/Services/Authentication/IAuthenticationService.cs
@@ -0,0 +1,9 @@
+namespace CleanArc.Application.Services.Authentication;
+
+public interface IAuthenticationService
+{
+    AuthenticationResult Login(string email, string password);
+
+    AuthenticationResult Register(string firstName, string lastName, string email, string password);
+}
+
